dbd65bfb0395c46d7e3e81cd133d26e3dc447ca6
Pedro Sanchez <pedrolampo50@gmail.com>
2021-11-23 16:14:08 -0300

    itemcounter updated

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,12 +5,9 @@ const ItemCount = () => {
     const [count, setCount] = useState(1);
     const stock = 17;
 
-    const neverNegative = (e) => {
-        let input = e.target;
-        console.log(input);
-
-        if (isNaN(input.value) || input.value <= 0) {
-            input.value = 1;
+    const neverNegative = () => {
+        if (count >= 0) {
+            setCount(1);
         }
     };
 
@@ -22,7 +19,7 @@ const ItemCount = () => {
             <input
                 className="itemCountInput"
                 type="number"
-                onChange={neverNegative}
+                onChange={() => neverNegative}
                 value={count}
             />
             <button className="buttonRight" onClick={() => setCount(count + 1)}>
@@ -40,7 +37,7 @@ const Card = (props) => {
                 <b>Item 1</b>
                 <p>$500</p>
                 <ItemCount />
-                <button>COMPRAR</button>
+                <button className="addToCartButton">Agregar al carrito</button>
             </div>
         </div>
     );
