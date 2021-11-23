f59c2f5ac378016702a5fb502c410a9db6d77369
Pedro Sanchez <pedrolampo50@gmail.com>
2021-11-23 20:05:11 -0300

    itemcount updated

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,27 +3,31 @@ import './Card.css';
 
 const ItemCount = () => {
     const [count, setCount] = useState(1);
-    const stock = 17;
+    const stock = 14;
 
     const less = () => {
         if (count === 1) {
-            alert('No se puede menos de 1');
             return;
         }
         setCount(count - 1);
     };
 
+    const stockLimit = () => {
+        if (count >= stock) {
+            return;
+        }
+        setCount(count + 1);
+    };
+
     return (
         <div className="countContainer">
-            <button
-                onClick={() => less()}
-                disabled={count === 1 ? 'disabled' : null}
-                className="Restar buttonLeft"
-            >
+            <button onClick={() => less()} className="buttonLeft">
                 -
             </button>
-            <input className="itemCountInput" type="number" value={count} />
-            <button className="buttonRight" onClick={() => setCount(count + 1)}>
+            <p className="itemCountInput" onChange={() => {}}>
+                {count}
+            </p>
+            <button className="buttonRight" onClick={() => stockLimit()}>
                 +
             </button>
         </div>
@@ -34,9 +38,11 @@ const Card = (props) => {
     return (
         <div className="card">
             <img src={props.image} alt="itemPhoto" />
-            <div>
-                <b>Item 1</b>
-                <p>$500</p>
+            <div className="itemTitle">
+                <b>{props.item}</b>
+                <p>{props.price}</p>
+            </div>
+            <div className="amountPurchase">
                 <ItemCount />
                 <button className="addToCartButton">Agregar al carrito</button>
             </div>
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -7,11 +7,11 @@ import item3 from '../Card/angus4.jpg';
 const ItemListContainer = () => {
     return (
         <section className="shopItems">
-            <Card image={item1} />
-            <Card image={item2} />
-            <Card image={item3} />
-            <Card image={item3} />
-            <Card image={item3} />
+            <Card image={item1} item="Item 1" price="$500" />
+            <Card image={item2} item="Item 2" price="$5400" />
+            <Card image={item3} item="Item 3" price="$300" />
+            <Card image={item3} item="Item 4" price="$500" />
+            <Card image={item3} item="Item 5" price="$100" />
         </section>
     );
 };
