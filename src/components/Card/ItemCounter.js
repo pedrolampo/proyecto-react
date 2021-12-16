54a0c9a6c0a99f018c9cdc81fd58ba3c33f27788
Pedro Sanchez <pedrolampo50@gmail.com>
2021-12-15 23:11:58 -0300

    added comment on itemcounter

--- src/components/Card/ItemCounter.js
+++ src/components/Card/ItemCounter.js
@@ -19,6 +19,7 @@ const ItemCount = (props) => {
         setCount(count + 1);
     };
 
+    // SUBIR ESTA FUNCION A ItemDetail
     const addToCart = () => {
         if (cartQty === 0) {
             setCartQty(count);
